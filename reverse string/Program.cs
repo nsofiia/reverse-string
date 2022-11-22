846c5794eb9b9f1b9053218f013dc8c8f0f6eb15
Sofi <nsofiia01@gmail.com>
2022-11-22 17:51:11 -0600

    finished: added string comparison

--- reverse string/Program.cs
+++ reverse string/Program.cs
@@ -1,34 +1,27 @@
-﻿namespace reverse_string;
+﻿using System.Linq;
+
+namespace reverse_string;
 class Program
 {
     static void Main(string[] args)
     {
 
         Console.WriteLine("Hello, I'll reverse anything you say: ");
-
-        string x = Console.ReadLine();
-
-        int y = x.Length-1;
-
-        char[] a = x.ToCharArray();
-
-        char[] b = {};
-
+        string initialInput = Console.ReadLine();//get input
+        int iterationsToReverse = initialInput.Length - 1;//ammount of iterations depends on the ammount of characters in the input
+        List<string> reversedInputList = new List<string> { };//list to collect reversed input frm the loop
+        Console.Write("reversed:");
         do
         {
-            Console.WriteLine(a[y]);
-            b.Append(a[y]);
-            y--;
-        } while (y > -1);
-
-        Console.ReadLine(b);
-
-
-        if (a == b)
+            Console.Write(initialInput[iterationsToReverse]);//prints string starting from the last index of input
+            reversedInputList.Add(initialInput[iterationsToReverse].ToString());//adding each printed above letter to the list 
+            iterationsToReverse--;
+        } while (iterationsToReverse > -1);
+        string reversedInput = string.Join("", reversedInputList);//joining list into string
+        if (initialInput == reversedInput) //comparing strings
         {
-            Console.WriteLine("This is polindrome!");
+            Console.WriteLine("\nfound palindrome!");
         }
         Console.ReadKey(true);
     }
 }
-
