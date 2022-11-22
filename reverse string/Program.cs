85a824e527d74c841380500e7fcbc0c098898a5c
Sofi <nsofiia01@gmail.com>
2022-11-21 19:11:16 -0600

    reverse string
    reverse string and find palindromes

---
+++ reverse string/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace reverse_string;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, I'll reverse anything you say: ");
+
+        string x = Console.ReadLine();
+
+        int y = x.Length-1;
+
+        char[] a = x.ToCharArray();
+
+        char[] b = {};
+
+        do
+        {
+            Console.WriteLine(a[y]);
+            b.Append(a[y]);
+            y--;
+        } while (y > -1);
+
+        Console.ReadLine(b);
+
+
+        if (a == b)
+        {
+            Console.WriteLine("This is polindrome!");
+        }
+        Console.ReadKey(true);
+    }
+}
+
