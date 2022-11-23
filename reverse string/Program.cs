6fb9525b3abfc6f7d284c13b8a7114e35ca14156
Sofi <nsofiia01@gmail.com>
2022-11-23 16:10:39 -0600

    logic updated: using stings only

--- reverse string/Program.cs
+++ reverse string/Program.cs
@@ -9,19 +9,31 @@ class Program
         Console.WriteLine("Hello, I'll reverse anything you say: ");
         string initialInput = Console.ReadLine();//get input
         int iterationsToReverse = initialInput.Length - 1;//ammount of iterations depends on the ammount of characters in the input
-        List<string> reversedInputList = new List<string> { };//list to collect reversed input frm the loop
-        Console.Write("reversed:");
-        do
+        string reversedStr = "";
+        while (iterationsToReverse > -1)
         {
-            Console.Write(initialInput[iterationsToReverse]);//prints string starting from the last index of input
-            reversedInputList.Add(initialInput[iterationsToReverse].ToString());//adding each printed above letter to the list 
+            reversedStr += initialInput[iterationsToReverse];
             iterationsToReverse--;
-        } while (iterationsToReverse > -1);
-        string reversedInput = string.Join("", reversedInputList);//joining list into string
-        if (initialInput.ToUpper() == reversedInput.ToUpper()) //comparing strings
+        }
+
+        if (initialInput.ToUpper().Equals(reversedStr.ToUpper()))
         {
-            Console.WriteLine("\nfound palindrome!");
+            Console.WriteLine("found palindrome");
         }
         Console.ReadKey(true);
+        //List<string> reversedInputList = new List<string> { };//list to collect reversed input frm the loop
+        //Console.Write("reversed:");
+        //do
+        //{
+        //    Console.Write(initialInput[iterationsToReverse]);//prints string starting from the last index of input
+        //   // reversedInputList.Add(initialInput[iterationsToReverse].ToString());//adding each printed above letter to the list 
+        //    iterationsToReverse--;
+        //} while (iterationsToReverse > -1);
+        //string reversedInput = string.Join("", reversedInputList);//joining list into string
+        //if (initialInput.ToUpper() == reversedInput.ToUpper()) //comparing strings
+        //{
+        //    Console.WriteLine("\nfound palindrome!");
+        //}
+        //Console.ReadKey(true);
     }
 }
