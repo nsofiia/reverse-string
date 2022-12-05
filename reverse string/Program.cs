fbf83ab5195be0cf80dee6c9326e52a65a74d698
Sofi <nsofiia01@gmail.com>
2022-12-04 21:26:22 -0600

    improved variable naming; lessened checks count for palindrome, when false

--- reverse string/Program.cs
+++ reverse string/Program.cs
@@ -6,25 +6,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, I'll reverse anything you say: ");
-        string initialInput = Console.ReadLine().ToLower();
-        int palindromeCount = 0;
-        int actualIndex = 0;
 
-        for (int index = initialInput.Length - 1; index > -1; index--)
+        string initialInput = Console.ReadLine().ToLower();
+        int lastIndex = initialInput.Length - 1;
+        bool isPalindrome = true;
+
+        for (int index = lastIndex; index > -1; index--)
         {
             Console.Write(initialInput[index]);
-            if (initialInput[index] == initialInput[actualIndex])
+            if (initialInput[index] != initialInput[lastIndex - index])
             {
-                actualIndex++;
-                palindromeCount++;
+                isPalindrome = false;
             }
-
-            if (palindromeCount == initialInput.Length)
-            {
-                Console.WriteLine("\nPalindrome");
-            }
+        }
+        if (isPalindrome)
+        {
+            Console.WriteLine("\nThis is a Palindrome");
         }
         Console.ReadLine();
     }
 }
-
