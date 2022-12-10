b7c9414ee305c12bd80cd0c68a517e1bb0b26f70
Sofi <nsofiia01@gmail.com>
2022-12-10 15:02:33 -0600

    added loop to restart the game

--- reverse string/Program.cs
+++ reverse string/Program.cs
@@ -5,24 +5,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, I'll reverse anything you say: ");
+        Console.WriteLine("Hello, I'll reverse anything you say:\nenter a palindrome, to activate the palindrome detection feature");
 
-        string initialInput = Console.ReadLine().ToLower();
-        int lastIndex = initialInput.Length - 1;
-        bool isPalindrome = true;
-
-        for (int index = lastIndex; index > -1; index--)
+        while (true)
         {
-            Console.Write(initialInput[index]);
-            if (initialInput[index] != initialInput[lastIndex - index])
+            Console.WriteLine("\nType in the word and press enter:\n");
+            string initialInput = Console.ReadLine().ToLower();
+            int lastIndex = initialInput.Length - 1;
+            bool isPalindrome = true; //assuming that it is a palindrome and then check
+
+            for (int index = lastIndex; index > -1; index--)
             {
-                isPalindrome = false;
+                Console.Write(initialInput[index]); //displaying the reversed order character by character
+                if (initialInput[index] != initialInput[lastIndex - index]) // palindrome check - if 1st letter = to last letter
+                {
+                    isPalindrome = false; //when at least 1 letter is not equal - it is not a palindrome
+                }
+            }
+            if (isPalindrome) //if palindrome check kept valie as true - it is a palindrome 
+            {
+                Console.WriteLine("\nThis is a Palindrome");
+            }
+
+            Console.WriteLine("\nWould you like to try another word? y- restart; any other key to exit\n");
+            char answer = Char.ToLower(Console.ReadKey().KeyChar);
+            if (answer=='y')
+            {
+                continue;
+            }
+            else
+            {
+                return;
             }
         }
-        if (isPalindrome)
-        {
-            Console.WriteLine("\nThis is a Palindrome");
-        }
-        Console.ReadLine();
     }
 }
